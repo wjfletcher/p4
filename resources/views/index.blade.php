df3b7d0c952835ea94bf1388b05139cd2f542821
Warren Fletcher <warrenjfletcher@gmail.com>
2015-12-18 16:59:28 -0500

    more html error fixing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
 <div class="panel panel-default">
     <div class="panel-body">
 
-        <h3><p>Welcome to BeerDrinker, where you can record all your beer drinking knowledge! Now, whenever you crack open a new beer, you can pop over to BeerDrinker and add it to your list of beers you've tried, including the beer name, a rating, as well as your description of that particular brew. Never again will you walk into a liquor store and wonder if you liked a beer or not!</p>
-            <p>But wait, there's more! Like visiting breweries? You can also save the ones you visit to BeerDrinker!</p>
-            <p>Simply login above (or register if this is your first time here), and select beers or breweries from the menu and start adding!</p></h3>
+        <h3>Welcome to BeerDrinker, where you can record all your beer drinking knowledge! Now, whenever you crack open a new beer, you can pop over to BeerDrinker and add it to your list of beers you've tried, including the beer name, a rating, as well as your description of that particular brew. Never again will you walk into a liquor store and wonder if you liked a beer or not!<br>
+            But wait, there's more! Like visiting breweries? You can also save the ones you visit to BeerDrinker!<br>
+            Simply login above (or register if this is your first time here), and select beers or breweries from the menu and start adding!</h3>
     </div>
 </div>
 @stop
